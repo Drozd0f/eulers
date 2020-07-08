8e3e1e83ea5073f3a5b7161fbea6361a2fc0d223
|\/| PAK |\/| <you@example.com>
2020-07-08 11:07:53 +0300

    add eulers_projects

---
+++ exercise_7.py
@@ -0,0 +1,42 @@
+# Выписав первые шесть простых чисел, получим 2, 3, 5, 7, 11 и 13. Очевидно, что 6-ое простое число - 13.
+# Какое число является 10001-ым простым числом?
+dead_inside = 2
+
+while True:
+    d = 6
+
+    print("dead inside = ", dead_inside)
+
+    a = []
+    for i in range(2, dead_inside + 1):
+        a.append(i)
+
+    # print("Наши числа", a)
+
+    b = []
+
+    for n in range(2, dead_inside + 1):
+        for k in range(n + 1, dead_inside + 1):
+            if k % n == 0:
+                b.append(k)
+
+    # print("Сложеные числа для каждого числа а", b)
+
+    c = list(set(b))
+
+    # print("Фильтруем сложеные числа", c)
+
+    for k in c:
+        for i in a:
+            if i == k:
+                a.remove(k)
+
+    if d == len(a):
+        print("Простые числа", a)
+        print("Простое число под номером", d, "это", a[d - 1])
+        break
+
+    else:
+        dead_inside += 1
+
+
---
+++ exercise_9.py
@@ -0,0 +1,25 @@
+# Тройка Пифагора - три натуральных числа a < b < c, для которых выполняется равенство
+# a^2 + b^2 = c^2
+# Например, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+# Существует только одна тройка Пифагора, для которой a + b + c = 1000.
+# Найдите произведение abc.
+summa = 0
+b = 2
+d = []
+while True:
+    for a in range(1, b):
+        c = (a ** 2 + b ** 2) ** (1 / 2)
+        if c == int(c):
+            #d.append(a)
+            #d.append(b)
+            #d.append(c)
+            summa = a + b + c
+            d.append(summa)
+            if summa == 1000:
+                print(a, "+", b, "+", c)
+                print(a * b * c)
+                break
+    if c >= 500:
+        break
+    b += 1
+print(d)
